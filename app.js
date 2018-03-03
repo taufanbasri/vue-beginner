656eb1be997fd3bfde815846aecd5524b4622824
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-03-03 21:45:46 +0700

    Event

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Taufan',
-        job: 'Web Developer',
-        web: 'http://github.com/taufanbasri',
-        webTag: '<a href="http://github.com/taufanbasri" target="_blank">Tag Binding</a>'
+        age: 25,
     },
     methods: {
         greet(time) {
