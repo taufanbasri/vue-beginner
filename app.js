a06b7b766a449f61d16dc4eae749fb92f2d4c9af
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-03-03 20:27:35 +0700

    Data Binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#app',
     data: {
         name: 'Taufan',
-        job: 'Web Developer'
+        job: 'Web Developer',
+        web: 'http://github.com/taufanbasri',
+        webTag: '<a href="http://github.com/taufanbasri" target="_blank">Tag Binding</a>'
     },
     methods: {
         greet(time) {
