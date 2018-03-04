5a313ca0d4e718314f287662eb33abf88c0b5942
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-03-04 21:07:01 +0700

    Components Basic

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-var one = new Vue({
-    el: '#app-one',
-    data: {
-       title: 'Vue App One'
-    },
-    
-    methods: {
-        
-    },
+Vue.component('greeting', {
+    template: '<p>Hey there, I am a re-usable component</p>'
+});
 
-    computed: {
-        greet() {
-            return 'Hello from app one :)'
-        }
-    }
-})
+var one = new Vue({
+    el: '#app-one'
+});
 
 var two = new Vue({
-    el: '#app-two',
-    data: {
-        title: 'Vue App Two'
-    },
-    
-    methods: {
-        changeTitle() {
-            one.title = 'Title Changed!'
-        }
-    },
-
-    computed: {
-        greet() {
-            return 'Hello from app two :)'
-        }
-    }
-})
-
-two.title = 'Change from outsite'
+    el: '#app-two'
+});
